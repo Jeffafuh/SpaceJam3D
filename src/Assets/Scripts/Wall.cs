cfa73a72baf1e216fdae2ff5bc1d144bcce84176
Rafael <rbgalvan@cougarnet.uh.edu>
2023-03-25 22:43:55 -0500

    Working moving walls

---
+++ src/Assets/Scripts/Wall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public float speed = 5f;
+    public GameObject CollisionWall;
+    void Start()
+    {
+        Debug.Log("New wall created");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, CollisionWall.transform.position, speed * Time.deltaTime); 
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject == CollisionWall)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void ProcureDubiousShape()
+    {
+
+    }
+}
---
+++ src/Assets/Scripts/WallSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpawner : MonoBehaviour
+{
+    public int wallCount = 5;
+    public float wallSpawnTimer = 3;
+    private float timer = 0;
+    public GameObject WallRef;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= wallSpawnTimer)
+        {
+            timer = 0;
+            GameObject temp = Instantiate(WallRef);
+            temp.SetActive(true);
+        }
+    }
+}
